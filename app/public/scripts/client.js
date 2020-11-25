10d128ebecf277d5cbe122c81b5f02b0e90f0def
Jordan Newberry <jordan.newberry@gmail.com>
2020-11-25 11:40:43 -0600

    code stuff

--- app/public/scripts/client.js
+++ app/public/scripts/client.js
@@ -24,22 +24,23 @@ function getPets() { // ajax GET function
 function renderPets(petData) {
   let display = $('#petTables');
   display.empty();
-  for (pet of petData) {
-    let $tr = $(`<tr data-id='${pet.id}'</tr>`);
-    $tr.data('pet', pet);
-    $tr.append(`<td class="name" data-name='${pet.name}'>${pet.name}</td>`);
-    $tr.append(`<td class="breed">${pet.breed}</td>`);
-    $tr.append(`<td class="color">${pet.color}</td>`);
-    $tr.append(
-      `<td class="checkIn" data-checkIn='${pet.checked_in}'>${pet.checked_in}</td>`
-    );
-    $tr.append(`<td class="notes">${pet.notes}</td>`);
-    $tr.append(
-      `<td class="td-checkIn"><button class='btn-checkIn'>Mark For checkIn</button></td>`
-    );
-    $tr.append(
-      `<td class="td-delete"><button class='btn-delete'>Delete</button></td>`
-    );
-    display.append($tr);
-  }
+  console.log('petData is...', petData[0].name);
+  // for (pet of petData) {
+  //   let $tr = $(`<tr data-id='${pet.id}'</tr>`);
+  //   $tr.data('pet', pet);
+  //   $tr.append(`<td class="name" data-name='${pet.name}'>${pet.name}</td>`);
+  //   $tr.append(`<td class="breed">${pet.breed}</td>`);
+  //   $tr.append(`<td class="color">${pet.color}</td>`);
+  //   $tr.append(
+  //     `<td class="checkIn" data-checkIn='${pet.checked_in}'>${pet.checked_in}</td>`
+  //   );
+  //   $tr.append(`<td class="notes">${pet.notes}</td>`);
+  //   $tr.append(
+  //     `<td class="td-checkIn"><button class='btn-checkIn'>Mark For checkIn</button></td>`
+  //   );
+  //   $tr.append(
+  //     `<td class="td-delete"><button class='btn-delete'>Delete</button></td>`
+  //   );
+  //   display.append($tr);
+  // }
 }
