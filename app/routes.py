ce21a3e27635de2024d2c650608c02e9e07889b8
Malik Elate <Malik.e1955@gmail.com>
2020-11-25 10:14:37 -0600

    added db connection

--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,7 @@
-from flask import request, redirect 
-from app import app 
+from flask import Flask, redirect, g, request 
+from app import app
+import psycopg2
+import psycopg2.pool
 
 @app.route('/')
 def index():
@@ -10,6 +12,24 @@ def index():
 def pets(): 
     return 'Pets' 
 
+app.config['postgreSQL_pool'] = psycopg2.pool.SimpleConnectionPool(
+    1, #min number of connections 
+    10, #max number of connections 
+    host = '127.0.0.1', 
+    port = '5432', 
+    database = 'pet_hotel' # Database name - this changes per project 
+)
 
+def get_db_conn(): 
+    if 'db' not in g: 
+        g.db = app.config['postgreSQL_pool'].getconn()
+        print('Got a connection')
+    return g.db
 
-
+# Close the database connection when done with a query 
+@app.teardown_appcontext 
+def close_db_conn(taco): 
+    db = g.pop('db', None)
+    if db is not None: 
+        app.config['postgreSQL_pool'].putconn(db)
+        print('Closing connection') 
